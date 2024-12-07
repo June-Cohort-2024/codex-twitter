ecde59ca1bb8066f8df99c6c62dc97fca1909217
frlopez88 <frlopez88@icloud.com>
2024-12-06 19:16:37 -0600

    Hash Browser

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { LogIn } from './ScreenComponents/LogIn'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {  Route, Routes , HashRouter } from 'react-router-dom'
 import { Wall } from './ScreenComponents/Wall'
 import { Error } from './ScreenComponents/Error'
 
@@ -10,13 +10,13 @@ function App() {
  
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<LogIn />}  /> 
           <Route path='/wall' element={  <Wall/> }  />
           <Route path='*' element = {<Error />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
